fa4064e5046aa9630c570a579d37db673f7ef07f
XDH-233 <wushanqiushui@gmail.com>
2022-12-23 21:10:44 +0800

    add ping-pong regs, Feature map buffer design and simulation

--- src/test/scala/R2Plus1D/InputBufferTest.scala
+++
@@ -1,7 +0,0 @@
-package R2Plus1D
-
-import Chainsaw.xilinx.VivadoSynth
-
-case class InputBufferTest() extends org.scalatest.flatspec.AnyFlatSpec {
-  "input buffer" should "consume 104 URAM" in VivadoSynth(InputBufer(), "input_buffer")
-}
